3b8b35decc451d26a52dfc836325e9bf2a1a4918
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2023-12-22 14:17:23 -0500

    chore: remove log, docker is working!!!

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
-console.log(process.env);
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3999);
